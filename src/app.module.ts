7fd09a2a3d5e2aae37120a9fcde243b6fc24d56d
Long Nguyen <47670554+longnnt@users.noreply.github.com>
2023-11-30 15:46:25 +0700

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -16,13 +16,11 @@ import { UserModule } from './user/user.module';
     // }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host:
-        'localhost' ||
-        'postgres://demo_clinic_user:rAxW4cQNEiorbogH2VAnmp4Ux5SW9Hct@dpg-clk4je6ukbfc7390s0q0-a/demo_clinic',
+      host: 'postgres://demo_clinic_user:rAxW4cQNEiorbogH2VAnmp4Ux5SW9Hct@dpg-clk4je6ukbfc7390s0q0-a/demo_clinic',
       port: 5432,
-      username: 'admin' || 'demo_clinic_user',
-      password: '412805@long' || 'rAxW4cQNEiorbogH2VAnmp4Ux5SW9Hct',
-      database: 'demo_clinic' || 'demo_clinic',
+      username: 'demo_clinic_user',
+      password: 'rAxW4cQNEiorbogH2VAnmp4Ux5SW9Hct',
+      database: 'demo_clinic',
       entities: ['dist/**/*.entity{.ts,.js}'],
       synchronize: true,
     }),
